6a2c568e9fc270159dc2711de7a8ec38ea5f5cc5
Christian Persson <chrissp_@hotmail.com>
2018-11-08 21:45:31 +0100

    started working on Position class, fields and props implemented

--- Labb2/Position.cs
+++ Labb2/Position.cs
@@ -3,6 +3,35 @@ namespace Labb2
 {
     public class Position
     {
+        private int x;
+        private int y;
+
+        public int X
+        {
+            get { return x; }
+            set
+            {
+                x = (value >= 0) ? value : 0;
+                //if (value >= 0)
+                //    x = value;
+
+                //x = 0;
+            }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set
+            {
+                y = (value >= 0) ? value : 0 ;
+                //if (value >= 0)
+                //    y = value;
+
+                //y = 0;
+            }
+        }
+
         public Position()
         {
         }
